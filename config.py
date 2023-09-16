8d4621d2b0d0dedebf36d2367c235a84d5b9e1d5
Bitrerbell <darkanger008@gmail.com>
2023-09-16 11:44:42 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = "6477511140:AAHEfFEQPEtJMyDWugg2yfp0oW-fmKSDv1s"
+API_ID = 10738943
+API_HASH = "da61e3a08b5ac78ce28b4a4cd854aeec"
+LOG_CHANNEL = -1001916137393
+AUTH_USERS = 5716978554
+DB_URL =  "mongodb+srv://adil00:12345@cluster0.fyt5yoa.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "BroadcastBot"
+BROADCAST_AS_COPY = True

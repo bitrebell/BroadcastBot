c50802bfe8254a4748c5c27cb13647780e705d6f
AADIL SHIEKH <114225192+bitrebell@users.noreply.github.com>
2023-09-16 12:37:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 BOT_TOKEN = "6477511140:AAHEfFEQPEtJMyDWugg2yfp0oW-fmKSDv1s"
 API_ID = 10738943
 API_HASH = "da61e3a08b5ac78ce28b4a4cd854aeec"
-LOG_CHANNEL = -1001916137393
-AUTH_USERS = 5716978554
+LOG_CHANNEL = -1001986317084
+AUTH_USERS = 6163840192
 DB_URL =  "mongodb+srv://adil00:12345@cluster0.fyt5yoa.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "BroadcastBot"
 BROADCAST_AS_COPY = True
